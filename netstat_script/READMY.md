93f0d356ba543042b9577ae236f42ba6c52d6bd3
sevod <ssevod@gmail.com>
2021-06-10 17:41:06 +0300

    netstat_script start

---
+++ netstat_script/READMY.md
@@ -0,0 +1,6 @@
+
+
+sudo netstat -tunapl | awk '/firefox/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+
+1 parameter - PID or name process.
+2 parameter - number of rows.
---
+++ netstat_script/netstat_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#made check for arguments. especially for 2
+
+nets=$(sudo netstat -tunapl)
+
+nets=$(awk '/'"$1"'/ {print $5}' <<< "$nets")
+
+nets=$(cut -d: -f1 <<< "$nets")
+
+nets=$(sort <<< "$nets")
+
+nets=$(uniq -c <<< "$nets")
+
+nets=$(sort <<< "$nets")
+
+nets=$(tail -n"$2" <<< "$nets") #need check 2 argument
+
+nets=$(grep -oP '(\d+\.){3}\d+' <<< "$nets")
+
+
+echo "$nets"
